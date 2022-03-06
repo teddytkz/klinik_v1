eb4a374f7294df2bc0723ed467262fcdf8ff73ea
teddytkz <tkzteddy@gmail.com>
2022-03-06 17:18:33 +0700

    Add Api Logout

--- pages/api/auth/logout.ts
+++ pages/api/auth/logout.ts
@@ -1,10 +1,27 @@
-// import prisma from '../../../lib/prisma'
+import { prisma } from '../../../lib/prisma'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
-    try {
-
-    } catch (error) {
-
+    if (req.method === 'POST') {
+        const { userToken } = req.cookies
+        try {
+            if (!userToken) {
+                return res.status(401).json({ message: "Token Not Found" })
+            }
+            const user = await prisma.user.findFirst({ where: { token: String(userToken) } })
+            const deleteToken = await prisma.user.update({
+                where: {
+                    id: Number(user?.id)
+                }, data: {
+                    token: ''
+                }
+            })
+            return res.status(200).json({ message: "Logout" })
+        } catch (err) {
+            return res.status(500).json({ message: "Error", data: err })
+        }
+    } else {
+        res.status(405).json({ message: "Method Not Allowed" })
     }
+
 }
