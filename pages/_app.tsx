1d4eabf295ebfcb1ed6eae31b4e743f9d8496253
Teddy Dee <tkzteddy@gmail.com>
2022-03-30 15:18:46 +0700

    Add Template Monster Admin

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,50 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import FullLayout from "../src/layouts/FullLayout";
+import Head from "next/head";
+import "../styles/style.scss";
+import { useRouter } from 'next/router'
+import { AppProps } from "next/dist/shared/lib/router/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+  const path = router.pathname.split("/")
+  console.log(path[1])
+  console.log(router.pathname)
+  if (path[1] === 'auth') {
+    return (
+      <>
+        <Head>
+          <title>Monster Free Next Js Dashboard Template</title>
+          <meta
+            name="description"
+            content="Monster Free Next Js Dashboard Template"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+
+        <Component {...pageProps} />
+
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Head>
+          <title>Monster Free Next Js Dashboard Template</title>
+          <meta
+            name="description"
+            content="Monster Free Next Js Dashboard Template"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <FullLayout>
+          <Component {...pageProps} />
+        </FullLayout>
+      </>
+    );
+  }
+
 }
 
-export default MyApp
+export default MyApp;
