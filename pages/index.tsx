6aa371a2b1683e235a0e83adf7b887085ff3be6e
Teddy Dee <tkzteddy@gmail.com>
2022-03-31 08:31:59 +0700

    Fix : Minor update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import Head from "next/head";
-import { Col, Row } from "reactstrap";
+import { Col, Row } from 'reactstrap';
 import SalesChart from "../src/components/dashboard/SalesChart";
 import Feeds from "../src/components/dashboard/Feeds";
 import ProjectTables from "../src/components/dashboard/ProjectTable";
 import TopCards from "../src/components/dashboard/TopCards";
 import Blog from "../src/components/dashboard/Blog";
-import bg1 from "../src/assets/images/bg/bg1.jpg";
+import bg1 from '../src/assets/images/bg/bg1.jpg';
 import bg2 from "../src/assets/images/bg/bg2.jpg";
 import bg3 from "../src/assets/images/bg/bg3.jpg";
 import bg4 from "../src/assets/images/bg/bg4.jpg";
