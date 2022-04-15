b83c909d13b68ec3869b5c70bebeb963d75e2764
Teddy Dee <tkzteddy@gmail.com>
2022-04-15 16:26:22 +0700

    Update : Login Page

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,6 +1,39 @@
 import type { NextPage } from 'next'
+import { useState } from 'react'
 
 const Login: NextPage = () => {
+    const [fields, setFields] = useState({
+        username: '',
+        password: ''
+    })
+
+    async function loginHandler(e: any) {
+        e.preventDefault()
+
+        const loginReq = await fetch('/api/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(fields)
+        })
+
+        if (!loginReq.ok) {
+
+        }
+
+        const loginRes = await loginReq.json()
+        console.log(loginRes.message)
+    }
+
+    function fieldHandler(e: any) {
+        const name = e.target.getAttribute('name')
+        setFields({
+            ...fields,
+            [name]: e.target.value
+        })
+    }
+
     return (
         <div className='App backgroundContainer' style={{
             backgroundImage: 'url(https://source.unsplash.com/random)',
@@ -14,19 +47,19 @@ const Login: NextPage = () => {
                             <div className='text-center'>
                                 <h3 className='text-primary'>Sign In</h3>
                             </div>
-                            <form>
+                            <form onSubmit={loginHandler.bind(this)}>
                                 <div className='p-4'>
                                     <div className='input-group mb-3'>
                                         <span className='input-group-text bg-primary'>
                                             <i className='bi bi-person-plus-fill text-white'></i>
                                         </span>
-                                        <input type='text' className='form-control' placeholder='Username' />
+                                        <input type='text' name='username' onChange={fieldHandler.bind(this)} className='form-control' placeholder='Username' />
                                     </div>
                                     <div className='input-group mb-3'>
                                         <span className='input-group-text bg-primary'>
                                             <i className='bi bi-key-fill text-white'></i>
                                         </span>
-                                        <input type='text' className='form-control' placeholder='Password' />
+                                        <input type='text' name='password' onChange={fieldHandler.bind(this)} className='form-control' placeholder='Password' />
                                     </div>
                                     <button className='btn btn-primary text-center mt-2' type='submit'>Login</button>
                                 </div>
