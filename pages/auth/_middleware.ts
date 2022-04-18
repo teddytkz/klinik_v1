b5fcedc3d3338e233c4d451437ddf231aa19b817
Teddy Dee <tkzteddy@gmail.com>
2022-04-18 11:37:32 +0700

    Add : Middleware On Login Page

--- pages/auth/_middleware.ts
+++ pages/auth/_middleware.ts
@@ -1,5 +1,15 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextRequest, NextResponse } from 'next/server'
 
-export default async function middleware(req: NextApiRequest, res: NextApiResponse) {
-    console.log('a')
+export default async function middleware(req: NextRequest) {
+    const { userToken } = req.cookies
+
+    const url = req.nextUrl.clone()
+
+    if (!userToken) {
+        url.pathname = '/auth/login'
+        return NextResponse.rewrite(url)
+    } else {
+        url.pathname = '/dashboard'
+        return NextResponse.rewrite(url)
+    }
 }
